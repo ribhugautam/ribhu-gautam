1866a33d8f80a26a894066e75b879a0f8845d1bd
ribhugautam <gautamribhu@gmail.com>
2024-03-17 01:52:16 +0530

    alpha

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <div className="flex flex-col gap-6 mt-24" >
-      <span className="text-cyan-400 font-handjet text-lg font-bold " >Hi, my name is</span>
+      <span className="text-cyan-400 font-handjet text-xl font-bold " >Hi, my name is</span>
 
       <div className="text-4xl lg:text-6xl xl:text-6xl font-poppins font-bold flex flex-col gap-2 " >
         <h1 className="text-slate-200" >Ribhu Gautam.</h1>
@@ -12,10 +12,10 @@ function Hero() {
       </div>
 
       <p className="font-poppins max-w-[480px] " >
-        As a software engineer specializing in frontend development, you focus
+        As a software engineer specializing in frontend development, I focus
         on designing and implementing the user interface and experience of web
         applications, ensuring they are functional, responsive, and
-        aesthetically pleasing to facilitate user <span className="text-cyan-400 font-handjet font-bold text-lg" >interaction.</span>
+        aesthetically pleasing to facilitate user <span className="text-cyan-400 font-handjet font-bold text-xl" >interaction.</span>
       </p>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,133 +5,109 @@ import { useState } from "react";
 
 function Navbar() {
   const [navClick, setNavClick] = useState(false);
+
+  const handleClick = () => {
+    const listElement = document.getElementById("list");
+    if (!listElement) {
+      console.error("No list element found!");
+      return;
+    }
+    if (listElement === null || window.matchMedia === null) {
+      console.error("listElement or window.matchMedia is null");
+      return;
+    }
+    if (
+      window.matchMedia("(min-width : 768px)").matches ||
+      window.matchMedia("(min-width : 1024px)").matches ||
+      window.matchMedia("(min-width : 1280px)").matches ||
+      window.matchMedia("(min-width : 1536px)").matches ||
+      window.matchMedia("(min-width : 1600px)").matches ||
+      window.matchMedia("(min-width : 1920px)").matches
+    ) {
+      return;
+    }
+    listElement.classList.toggle("translate-x-[100%]");
+    listElement.classList.toggle("hidden");
+    const htmlElement = document.querySelector("html");
+    if (!htmlElement) {
+      console.error("No HTML element found!");
+      return;
+    }
+    htmlElement.classList.toggle("overflow-hidden");
+    const bodElement = document.querySelector("#bod");
+    if (!bodElement) {
+      console.error("No bod element found!");
+      return;
+    }
+    bodElement.classList.toggle("blur-sm");
+    setNavClick(!navClick);
+  };
+
+  const handleClose = () => {
+    setNavClick(!navClick);
+    const listElement = document.getElementById("list");
+    if (listElement) {
+      listElement.classList.toggle("translate-x-[100%]");
+      listElement.classList.toggle("hidden");
+
+      const html = document.querySelector("html");
+      const body = document.querySelector("#bod");
+      if (html) {
+        html.classList.toggle("overflow-hidden");
+        body.classList.toggle("blur-sm");
+      } else {
+        console.error("Could not find html element");
+      }
+    } else {
+      console.error("Could not find element with id 'list'");
+    }
+  };
+
   return (
     <div className="w-11/12 max-w-[1180px] mx-auto ">
       <div className="flex justify-between py-8 items-center">
         <div className="relative">
-            <Link to="/" >
-          <div className="border-4 border-cyan-400 size-10 rounded-l-full rounded-t-full" />
-          <span className="absolute top-1 left-[16px] font-handjet font-bold text-2xl text-cyan-400 ">
-            R
-          </span>
+          <Link to="/">
+            <div className="border-4 border-cyan-400 size-10 rounded-l-full rounded-t-full" />
+            <span className="absolute top-1 left-[16px] font-handjet font-bold text-2xl text-cyan-400 ">
+              R
+            </span>
           </Link>
         </div>
-        
+
         <div>
           <ul
             id="list"
             className={` absolute flex hidden z-[5] right-0 top-0 p-24 h-screen w-[80%] translate-x-[100%] md:translate-x-0 lg:translate-x-0 xl:translate-x-0 md:w-auto lg:w-auto xl:w-auto md:h-auto lg:h-auto xl:h-auto md:p-0 lg:p-0 xl:p-0 bg-slate-950 md:bg-transparent lg:bg-transparent xl:bg-transparent font-handjet md:static lg:static xl:static flex-col md:flex md:flex-row lg:flex lg:flex-row xl:flex xl:flex-row justify-center md:text-xs lg:text-xs xl:text-sm transition-all ease-in-out duration-200 font-medium items-center gap-10 md:gap-8 lg:gap-8 xl:gap-8 `}
           >
-            <HashLink
-              onClick={() => {
-                setNavClick(!navClick);
-                document
-                  .getElementById("list")
-                  .classList.toggle("translate-x-[100%]");
-                  document
-                  .getElementById("list")
-                  .classList.toggle("hidden");
-                document
-                  .querySelector("html")
-                  .classList.toggle("overflow-hidden");
-                document.querySelector("#bod").classList.toggle("blur-sm");
-              }}
-              to="/about/#about"
-            >
-              <li className="flex gap-2 cursor-pointer hover:text-cyan-400 transition-all duration-200 ">
-                <span className="text-cyan-400 font-handjet font-bold ">
-                  01.
-                </span>{" "}
-                About{" "}
+            <HashLink onClick={handleClick} to="/about/#about">
+              <li className="flex gap-2 cursor-pointer font-handjet text-xl font-bold hover:text-cyan-400 transition-all duration-200 ">
+                <span className="text-cyan-400 font-handjet ">01.</span> About{" "}
               </li>
             </HashLink>
-            <HashLink
-              onClick={() => {
-                setNavClick(!navClick);
-                document
-                  .getElementById("list")
-                  .classList.toggle("translate-x-[100%]");
-                  document
-                  .getElementById("list")
-                  .classList.toggle("hidden");
-                document
-                  .querySelector("html")
-                  .classList.toggle("overflow-hidden");
-                document.querySelector("#bod").classList.toggle("blur-sm");
-              }}
-              to="/experience/#experience"
-            >
-              <li className="flex gap-2 cursor-pointer hover:text-cyan-400 transition-all duration-200 ">
-                <span className="text-cyan-400 font-handjet font-bold ">
-                  02.
-                </span>{" "}
+            <HashLink onClick={handleClick} to="/experience/#experience">
+              <li className="flex gap-2 cursor-pointer font-handjet text-xl font-bold hover:text-cyan-400 transition-all duration-200 ">
+                <span className="text-cyan-400 font-handjet ">02.</span>{" "}
                 Experience{" "}
               </li>
             </HashLink>
-            <HashLink
-              onClick={() => {
-                setNavClick(!navClick);
-                document
-                  .getElementById("list")
-                  .classList.toggle("translate-x-[100%]");
-                  document
-                  .getElementById("list")
-                  .classList.toggle("hidden");
-                document
-                  .querySelector("html")
-                  .classList.toggle("overflow-hidden");
-                document.querySelector("#bod").classList.toggle("blur-sm");
-              }}
-              to="/projects/#projects"
-            >
-              <li className="flex gap-2 cursor-pointer hover:text-cyan-400 transition-all duration-200 ">
-                <span className="text-cyan-400 font-handjet font-bold ">
-                  03.
-                </span>{" "}
+            <HashLink onClick={handleClick} to="/projects/#projects">
+              <li className="flex gap-2 cursor-pointer font-handjet text-xl font-bold hover:text-cyan-400 transition-all duration-200 ">
+                <span className="text-cyan-400 font-handjet ">03.</span>{" "}
                 Projects{" "}
               </li>
             </HashLink>
-            <HashLink
-              onClick={() => {
-                setNavClick(!navClick);
-                document
-                  .getElementById("list")
-                  .classList.toggle("translate-x-[100%]");
-                  document
-                  .getElementById("list")
-                  .classList.toggle("hidden");
-                document
-                  .querySelector("html")
-                  .classList.toggle("overflow-hidden");
-                document.querySelector("#bod").classList.toggle("blur-sm");
-              }}
-              to="/contact/#contact"
-            >
-              <li className="flex gap-2 cursor-pointer hover:text-cyan-400 transition-all duration-200 ">
-                <span className="text-cyan-400 font-handjet font-bold ">
-                  04.
-                </span>{" "}
-                Contact{" "}
+            <HashLink onClick={handleClick} to="/contact/#contact">
+              <li className="flex gap-2 cursor-pointer font-handjet text-xl font-bold hover:text-cyan-400 transition-all duration-200 ">
+                <span className="text-cyan-400 font-handjet ">04.</span> Contact{" "}
               </li>
             </HashLink>
             <Link
-              onClick={() => {
-                setNavClick(!navClick);
-                document
-                  .getElementById("list")
-                  .classList.toggle("translate-x-[100%]");
-                document
-                  .getElementById("list")
-                  .classList.toggle("hidden");
-                document
-                  .querySelector("html")
-                  .classList.toggle("overflow-hidden");
-                document.querySelector("#bod").classList.toggle("blur-sm");
-              }}
+              onClick={handleClick}
               to={"https://ufile.io/gav22l7f"}
               target="_blank"
             >
-              <li className="text-cyan-400 text-lg font-handjet font-bold border cursor-pointer border-cyan-400 p-1 px-4 rounded ">
+              <li className="text-cyan-400 text-xl font-handjet font-bold border cursor-pointer border-cyan-400 p-1 px-4 rounded ">
                 Resume
               </li>
             </Link>
@@ -139,25 +115,7 @@ function Navbar() {
         </div>
 
         <div
-          onClick={() => {
-            setNavClick(!navClick);
-            const listElement = document.getElementById("list");
-            if (listElement) {
-              listElement.classList.toggle("translate-x-[100%]");
-              listElement.classList.toggle("hidden");
-
-              const html = document.querySelector("html");
-              const body = document.querySelector("#bod");
-              if (html) {
-                html.classList.toggle("overflow-hidden");
-                body.classList.toggle("blur-sm");
-              } else {
-                console.error("Could not find html element");
-              }
-            } else {
-              console.error("Could not find element with id 'list'");
-            }
-          }}
+          onClick={handleClose}
           className={`flex cursor-pointer md:hidden lg:hidden xl:hidden z-10 gap-4`}
         >
           <div className="flex flex-col gap-[8px]">
